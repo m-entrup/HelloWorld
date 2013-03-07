b81f7c154ebe75df5b9a61bf635563743cf094c2
Michael Epping <m.epping@arcor.de>
2013-03-07 10:55:14 +0100

    Added HelloWorld.java
    Gibt "Hello World!" in der Console aus.

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Gibt "Hello World!" in der Console aus. 
+ * 
+ * @author m_eppi02
+ */
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+    }
+
+}
