33ba25def0b5e6658e167514752e4b1785cda44d
Michael Epping <m.epping@arcor.de>
2013-03-07 10:56:17 +0100

    Updated HelloWorld.java
    Deutsch statt Englisch.

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
      * @param args
      */
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+	System.out.println("Hallo Welt!");
     }
 
 }
